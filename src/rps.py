5f54b3a43afec509ceb895c463d880a60a3bc285
Sean Wieser <seanwieser@gmail.com>
2020-08-25 10:08:27 -0700

    updating to new archetecture

--- src/rps.py
+++ src/rps.py
@@ -8,133 +8,214 @@ from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
-from tensorflow.keras.datasets import mnist
-from tensorflow.keras.utils import to_categorical
-import numpy as np
 import matplotlib.pyplot as plt
 from skimage import io, color, filters
 from skimage.transform import resize, rotate
 import os
+from tensorflow.keras import applications
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras import optimizers
 
 import numpy as np
 np.random.seed(1337)  # for reproducibility
 
-def load_images(dir_name, data_kind):
-    img_lst = []
-    base_path = 'rps_images/' + data_kind
-    for filename in os.listdir(base_path + '/' + dir_name):
-        img_lst.append(io.imread(base_path + '/' + dir_name + '/' + filename))
-    return img_lst
 
-def load_and_featurize_data():
-    # train data 
-    paper_train = load_images('paper', 'train')
-    rock_train = load_images('rock', 'train')
-    sc_train= load_images('scissors', 'train')
-    X_train = np.array(paper_train + rock_train  + sc_train)
+def save_bottlebeck_features():
+    datagen = ImageDataGenerator(rescale=1. / 255)
 
-    # test data
-    paper_test = load_images('paper', 'test')
-    rock_test = load_images('rock', 'test')
-    sc_test = load_images('scissors', 'test')
-    X_test = np.array(paper_test + rock_test  + sc_test)
+    # build the VGG16 network
+    model = applications.VGG16(include_top=False, weights='imagenet')
 
-    # reshape input into format Conv2D layer likes
-    X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, channels)
-    X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, channels)
+    generator = datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode=None,
+        shuffle=False)
+    bottleneck_features_train = model.predict_generator(
+        generator, nb_train_samples // batch_size)
+    np.save(open('bottleneck_features_train.npy', 'w'),
+            bottleneck_features_train)
 
-    # don't change conversion or normalization
-    X_train = X_train.astype('float32')  # data was uint8 [0-255]
-    X_test = X_test.astype('float32')    # data was uint8 [0-255]
-    X_train /= 255  # normalizing (scaling from 0 to 1)
-    X_test /= 255   # normalizing (scaling from 0 to 1)
-
-
-    print('X_train shape:', X_train.shape)
-    print(X_train.shape[0], 'train samples')
-    print(X_test.shape[0], 'test samples')
-
-    y_train = np.array([3*i//X_train.shape[0] for i in range(X_train.shape[0])])
-    y_test = np.array([3*i//X_test.shape[0] for i in range(X_test.shape[0])])
-
-    # convert class vectors to binary class matrices
-    Y_train = to_categorical(y_train, nb_classes)  # cool
-    Y_test = to_categorical(y_test, nb_classes)
-    
-    # Shuffle all data
-    splits = [X_train, X_test, y_train, y_test]
-    rng_state = np.random.get_state()
-    for split in splits:
-        np.random.shuffle(split)
-        np.random.set_state(rng_state)
+    generator = datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode=None,
+        shuffle=False)
+    bottleneck_features_validation = model.predict_generator(
+        generator, nb_validation_samples // batch_size)
+    np.save(open('bottleneck_features_validation.npy', 'w'),
+            bottleneck_features_validation)
 
 
-    return X_train, X_test, Y_train, Y_test
+def train_top_model():
+    train_data = np.load(open('bottleneck_features_train.npy'))
+    train_labels = np.array(
+        [0] * (nb_train_samples / 2) + [1] * (nb_train_samples / 2))
 
+    validation_data = np.load(open('bottleneck_features_validation.npy'))
+    validation_labels = np.array(
+        [0] * (nb_validation_samples / 2) + [1] * (nb_validation_samples / 2))
 
-def define_model(nb_filters, kernel_size, input_shape, pool_size):
-    model = Sequential()  # model is a linear stack of layers (don't change)
+    model = Sequential()
+    model.add(Flatten(input_shape=train_data.shape[1:]))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1, activation='sigmoid'))
 
-    # note: the convolutional layers and dense layers require an activation function
-    # see https://keras.io/activations/
-    # and https://en.wikipedia.org/wiki/Activation_function
-    # options: 'linear', 'sigmoid', 'tanh', 'relu', 'softplus', 'softsign'
+    model.compile(optimizer='rmsprop',
+                  loss='binary_crossentropy', metrics=['accuracy'])
 
-    model.add(Conv2D(nb_filters,
-                     (kernel_size[0], kernel_size[1]),
-                     padding='valid',
-                     input_shape=input_shape))  # first conv. layer  KEEP
-    model.add(Activation('relu'))  # Activation specification necessary for Conv2D and Dense layers
+    model.fit(train_data, train_labels,
+              epochs=epochs,
+              batch_size=batch_size,
+              validation_data=(validation_data, validation_labels))
+    model.save_weights(top_model_weights_path)
 
-    model.add(Conv2D(nb_filters,
-                     (kernel_size[0], kernel_size[1]),
-                     padding='valid'))  # 2nd conv. layer KEEP
-    model.add(Activation('relu'))
-
-    # model.add(MaxPooling2D(pool_size=pool_size))  # decreases size, helps prevent overfitting
-    # model.add(Dropout(0.1))  # zeros out some fraction of inputs, helps prevent overfitting
-
-    model.add(Flatten())  # necessary to flatten before going into conventional dense layer  KEEP
-    print('Model flattened out to ', model.output_shape)
-
-    # now start a typical neural network
-    model.add(Dense(32))  # (only) 32 neurons in this layer, really?   KEEP
-    model.add(Activation('relu'))
-
-    # model.add(Dropout(0.5))  # zeros out some fraction of inputs, helps prevent overfitting
-
-    model.add(Dense(nb_classes))  # 10 final nodes (one for each class)  KEEP
-    model.add(Activation('softmax'))  # softmax at end to pick between classes 0-9 KEEP
-
-    # many optimizers available, see https://keras.io/optimizers/#usage-of-optimizers
-    # suggest you KEEP loss at 'categorical_crossentropy' for this multiclass problem,
-    # and KEEP metrics at 'accuracy'
-    # suggest limiting optimizers to one of these: 'adam', 'adadelta', 'sgd'
-    model.compile(loss='categorical_crossentropy',
-                  optimizer='adam',
-                  metrics=['accuracy'])
-    return model
 
 
 if __name__ == '__main__':
-    # important inputs to the model: don't changes the ones marked KEEP
-    batch_size = 20 # number of training samples used at a time to update the weights
-    nb_classes = 3    # number of output possibilities: [0 - 2] KEEP
-    nb_epoch = 1       # number of passes through the entire train dataset before weights "final"
-    img_rows, img_cols, channels = 300, 300, 4   # the size of the MNIST images KEEP
-    input_shape = (img_rows, img_cols, channels)   # 1 channel image input (grayscale) KEEP
-    nb_filters = 16  # number of convolutional filters to use
-    pool_size = (2, 2)  # pooling decreases image size, reduces computation, adds translational invariance
-    kernel_size = (6, 6)  # convolutional kernel size, slides over image to learn features
+    # dimensions of our images.
+    img_width, img_height = 300, 300
 
-    X_train, X_test, Y_train, Y_test = load_and_featurize_data()
+    top_model_weights_path = 'bottleneck_fc_model.h5'
+    train_data_dir = '../data/train'
+    validation_data_dir = '../data/validation'
+    nb_train_samples = 840
+    nb_validation_samples = 124
+    epochs = 50
+    batch_size = 16
+    save_bottlebeck_features()
+    train_top_model()
 
-    model = define_model(nb_filters, kernel_size, input_shape, pool_size)
 
-    # during fit process watch train and test error simultaneously
-    model.fit(X_train, Y_train, batch_size=batch_size, epochs=nb_epoch,
-              verbose=1, validation_data=(X_test, Y_test))
 
-    score = model.evaluate(X_test, Y_test, verbose=0)
-    print('Test score:', score[0])
-    print('Test accuracy:', score[1])  # this is the one we care about
+    # path to the model weights files.
+    weights_path = '../data/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5'
+    top_model_weights_path = 'fc_model.h5'
+    # dimensions of our images.
+    img_width, img_height = 150, 150
+
+    train_data_dir = 'cats_and_dogs_small/train'
+    validation_data_dir = 'cats_and_dogs_small/validation'
+    nb_train_samples = 2000
+    nb_validation_samples = 800
+    epochs = 50
+    batch_size = 16
+
+    # build the VGG16 network
+    model = applications.VGG16(weights='imagenet', include_top=False)
+    print('Model loaded.')
+
+    # build a classifier model to put on top of the convolutional model
+    top_model = Sequential()
+    top_model.add(Flatten(input_shape=model.output_shape[1:]))
+    top_model.add(Dense(256, activation='relu'))
+    top_model.add(Dropout(0.5))
+    top_model.add(Dense(1, activation='sigmoid'))
+
+    # note that it is necessary to start with a fully-trained
+    # classifier, including the top classifier,
+    # in order to successfully do fine-tuning
+    top_model.load_weights(top_model_weights_path)
+
+    # add the model on top of the convolutional base
+    model.add(top_model)
+
+    # set the first 25 layers (up to the last conv block)
+    # to non-trainable (weights will not be updated)
+    for layer in model.layers[:25]:
+        layer.trainable = False
+
+    # compile the model with a SGD/momentum optimizer
+    # and a very slow learning rate.
+    model.compile(loss='binary_crossentropy',
+                optimizer=optimizers.SGD(lr=1e-4, momentum=0.9),
+                metrics=['accuracy'])
+
+    # prepare data augmentation configuration
+    train_datagen = ImageDataGenerator(
+        rescale=1. / 255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+    test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+    train_generator = train_datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_height, img_width),
+        batch_size=batch_size,
+        class_mode='binary')
+
+    validation_generator = test_datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_height, img_width),
+        batch_size=batch_size,
+        class_mode='binary')
+
+    # fine-tune the model
+    model.fit_generator(
+        train_generator,
+        samples_per_epoch=nb_train_samples,
+        epochs=epochs,
+        validation_data=validation_generator,
+        nb_val_samples=nb_validation_samples)
+
+
+
+
+
+
+# def load_images(dir_name, data_kind):
+#     img_lst = []
+#     base_path = 'rps_images/' + data_kind
+#     for filename in os.listdir(base_path + '/' + dir_name):
+#         img_lst.append(io.imread(base_path + '/' + dir_name + '/' + filename))
+#     return img_lst
+
+# def load_and_featurize_data():
+#     # train data 
+#     paper_train = load_images('paper', 'train')
+#     rock_train = load_images('rock', 'train')
+#     sc_train= load_images('scissors', 'train')
+#     X_train = np.array(paper_train + rock_train  + sc_train)
+
+#     # test data
+#     paper_test = load_images('paper', 'test')
+#     rock_test = load_images('rock', 'test')
+#     sc_test = load_images('scissors', 'test')
+#     X_test = np.array(paper_test + rock_test  + sc_test)
+
+#     # reshape input into format Conv2D layer likes
+#     X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, channels)
+#     X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, channels)
+
+#     # don't change conversion or normalization
+#     X_train = X_train.astype('float32')  # data was uint8 [0-255]
+#     X_test = X_test.astype('float32')    # data was uint8 [0-255]
+#     X_train /= 255  # normalizing (scaling from 0 to 1)
+#     X_test /= 255   # normalizing (scaling from 0 to 1)
+
+
+#     print('X_train shape:', X_train.shape)
+#     print(X_train.shape[0], 'train samples')
+#     print(X_test.shape[0], 'test samples')
+
+#     y_train = np.array([3*i//X_train.shape[0] for i in range(X_train.shape[0])])
+#     y_test = np.array([3*i//X_test.shape[0] for i in range(X_test.shape[0])])
+
+#     # convert class vectors to binary class matrices
+#     Y_train = to_categorical(y_train, nb_classes)  # cool
+#     Y_test = to_categorical(y_test, nb_classes)
+    
+#     # Shuffle all data
+#     splits = [X_train, X_test, y_train, y_test]
+#     rng_state = np.random.get_state()
+#     for split in splits:
+#         np.random.shuffle(split)
+#         np.random.set_state(rng_state)
+
+
+#     return X_train, X_test, Y_train, Y_test
