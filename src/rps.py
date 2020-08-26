cefce50cf697c16b2f0e2ff55f2fde4e31e866c4
Sean Wieser <seanwieser@gmail.com>
2020-08-26 08:38:17 -0700

    wed morning

--- src/rps.py
+++ src/rps.py
@@ -25,43 +25,52 @@ def save_bottlebeck_features():
 
     # build the VGG16 network
     model = applications.VGG16(include_top=False, weights='imagenet')
-
+    print('Generating Train Images...')
     generator = datagen.flow_from_directory(
         train_data_dir,
         target_size=(img_width, img_height),
         batch_size=batch_size,
         class_mode=None,
         shuffle=False)
-    bottleneck_features_train = model.predict_generator(
-        generator, nb_train_samples // batch_size)
-    np.save(open('bottleneck_features_train.npy', 'w'),
-            bottleneck_features_train)
+    
+    print('Predicting Train')
+    bottleneck_features_train = model.predict(
+        generator, batch_size=nb_train_samples // batch_size, verbose=1)
 
+    np.save(open('bottleneck_features_train.npy', 'wb'),
+            bottleneck_features_train)
+    print('Train Bottleneck Features Saved...')
+
+    print('Generating Test Images')
     generator = datagen.flow_from_directory(
         validation_data_dir,
         target_size=(img_width, img_height),
         batch_size=batch_size,
         class_mode=None,
         shuffle=False)
-    bottleneck_features_validation = model.predict_generator(
-        generator, nb_validation_samples // batch_size)
-    np.save(open('bottleneck_features_validation.npy', 'w'),
+
+    print('Predicting Test')
+    bottleneck_features_validation = model.predict(
+        generator, batch_size=nb_validation_samples // batch_size, verbose=1)
+
+    np.save(open('bottleneck_features_validation.npy', 'wb'),
             bottleneck_features_validation)
+    print('Test Bottleneck Features Saved...')
 
 
 def train_top_model():
-    train_data = np.load(open('bottleneck_features_train.npy'))
-    train_labels = np.array(
-        [0] * (nb_train_samples / 2) + [1] * (nb_train_samples / 2))
+    train_data = np.load(open('bottleneck_features_train.npy', 'rb'))
+    train_num, val_num = 840, 124
+    train_labels = np.array([0]*train_num + [1]*train_num + [2]*train_num)
 
-    validation_data = np.load(open('bottleneck_features_validation.npy'))
-    validation_labels = np.array(
-        [0] * (nb_validation_samples / 2) + [1] * (nb_validation_samples / 2))
+    validation_data = np.load(open('bottleneck_features_validation.npy', 'rb'))
+    validation_labels = np.array([0]*val_num + [1]*val_num + [2]*val_num)
 
+    print('Building Model')
     model = Sequential()
     model.add(Flatten(input_shape=train_data.shape[1:]))
     model.add(Dense(256, activation='relu'))
-    model.add(Dropout(0.5))
+    # model.add(Dropout(0.5))
     model.add(Dense(1, activation='sigmoid'))
 
     model.compile(optimizer='rmsprop',
@@ -79,9 +88,9 @@ if __name__ == '__main__':
     # dimensions of our images.
     img_width, img_height = 300, 300
 
-    top_model_weights_path = 'bottleneck_fc_model.h5'
+    top_model_weights_path = '../data/bottleneck_fc_model.h5'
     train_data_dir = '../data/train'
-    validation_data_dir = '../data/validation'
+    validation_data_dir = '../data/test'
     nb_train_samples = 840
     nb_validation_samples = 124
     epochs = 50
@@ -92,76 +101,76 @@ if __name__ == '__main__':
 
 
     # path to the model weights files.
-    weights_path = '../data/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5'
-    top_model_weights_path = 'fc_model.h5'
+    # weights_path = '../data/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5'
+    # top_model_weights_path = 'fc_model.h5'
     # dimensions of our images.
-    img_width, img_height = 150, 150
+    # img_width, img_height = 150, 150
 
-    train_data_dir = 'cats_and_dogs_small/train'
-    validation_data_dir = 'cats_and_dogs_small/validation'
-    nb_train_samples = 2000
-    nb_validation_samples = 800
-    epochs = 50
-    batch_size = 16
+    # train_data_dir = 'cats_and_dogs_small/train'
+    # validation_data_dir = 'cats_and_dogs_small/validation'
+    # nb_train_samples = 2000
+    # nb_validation_samples = 800
+    # epochs = 50
+    # batch_size = 16
 
-    # build the VGG16 network
-    model = applications.VGG16(weights='imagenet', include_top=False)
-    print('Model loaded.')
+    # # build the VGG16 network
+    # model = applications.VGG16(weights='imagenet', include_top=False)
+    # print('Model loaded.')
 
-    # build a classifier model to put on top of the convolutional model
-    top_model = Sequential()
-    top_model.add(Flatten(input_shape=model.output_shape[1:]))
-    top_model.add(Dense(256, activation='relu'))
-    top_model.add(Dropout(0.5))
-    top_model.add(Dense(1, activation='sigmoid'))
+    # # build a classifier model to put on top of the convolutional model
+    # top_model = Sequential()
+    # top_model.add(Flatten(input_shape=model.output_shape[1:]))
+    # top_model.add(Dense(256, activation='relu'))
+    # top_model.add(Dropout(0.5))
+    # top_model.add(Dense(1, activation='sigmoid'))
 
-    # note that it is necessary to start with a fully-trained
-    # classifier, including the top classifier,
-    # in order to successfully do fine-tuning
-    top_model.load_weights(top_model_weights_path)
+    # # note that it is necessary to start with a fully-trained
+    # # classifier, including the top classifier,
+    # # in order to successfully do fine-tuning
+    # top_model.load_weights(top_model_weights_path)
 
-    # add the model on top of the convolutional base
-    model.add(top_model)
+    # # add the model on top of the convolutional base
+    # model.add(top_model)
 
-    # set the first 25 layers (up to the last conv block)
-    # to non-trainable (weights will not be updated)
-    for layer in model.layers[:25]:
-        layer.trainable = False
+    # # set the first 25 layers (up to the last conv block)
+    # # to non-trainable (weights will not be updated)
+    # for layer in model.layers[:25]:
+    #     layer.trainable = False
 
-    # compile the model with a SGD/momentum optimizer
-    # and a very slow learning rate.
-    model.compile(loss='binary_crossentropy',
-                optimizer=optimizers.SGD(lr=1e-4, momentum=0.9),
-                metrics=['accuracy'])
+    # # compile the model with a SGD/momentum optimizer
+    # # and a very slow learning rate.
+    # model.compile(loss='binary_crossentropy',
+    #             optimizer=optimizers.SGD(lr=1e-4, momentum=0.9),
+    #             metrics=['accuracy'])
 
-    # prepare data augmentation configuration
-    train_datagen = ImageDataGenerator(
-        rescale=1. / 255,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True)
+    # # prepare data augmentation configuration
+    # train_datagen = ImageDataGenerator(
+    #     rescale=1. / 255,
+    #     shear_range=0.2,
+    #     zoom_range=0.2,
+    #     horizontal_flip=True)
 
-    test_datagen = ImageDataGenerator(rescale=1. / 255)
+    # test_datagen = ImageDataGenerator(rescale=1. / 255)
 
-    train_generator = train_datagen.flow_from_directory(
-        train_data_dir,
-        target_size=(img_height, img_width),
-        batch_size=batch_size,
-        class_mode='binary')
+    # train_generator = train_datagen.flow_from_directory(
+    #     train_data_dir,
+    #     target_size=(img_height, img_width),
+    #     batch_size=batch_size,
+    #     class_mode='binary')
 
-    validation_generator = test_datagen.flow_from_directory(
-        validation_data_dir,
-        target_size=(img_height, img_width),
-        batch_size=batch_size,
-        class_mode='binary')
+    # validation_generator = test_datagen.flow_from_directory(
+    #     validation_data_dir,
+    #     target_size=(img_height, img_width),
+    #     batch_size=batch_size,
+    #     class_mode='binary')
 
-    # fine-tune the model
-    model.fit_generator(
-        train_generator,
-        samples_per_epoch=nb_train_samples,
-        epochs=epochs,
-        validation_data=validation_generator,
-        nb_val_samples=nb_validation_samples)
+    # # fine-tune the model
+    # model.fit_generator(
+    #     train_generator,
+    #     samples_per_epoch=nb_train_samples,
+    #     epochs=epochs,
+    #     validation_data=validation_generator,
+    #     nb_val_samples=nb_validation_samples)
 
 
 
