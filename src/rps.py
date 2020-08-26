f6694bf70bb60569bd581c9f584bbd65104adf4f
Sean Wieser <seanwieser@gmail.com>
2020-08-26 13:10:35 -0700

    changed to 3 nuerons on last layer for aws

--- src/rps.py
+++ src/rps.py
@@ -71,7 +71,7 @@ def train_top_model():
     model.add(Flatten(input_shape=train_data.shape[1:]))
     model.add(Dense(256, activation='relu'))
     # model.add(Dropout(0.5))
-    model.add(Dense(1, activation='softmax'))
+    model.add(Dense(3, activation='softmax'))
 
     model.compile(optimizer='adam',
                   loss='categorical_crossentropy', metrics=['accuracy'])
