2940d141819b884acac4504b7b4e454e669b8732
Sean Wieser <seanwieser@gmail.com>
2020-08-27 16:58:42 -0700

    sobel training

--- src/rps_scratch.py
+++ src/rps_scratch.py
@@ -70,7 +70,7 @@ if __name__ == "__main__":
     img_width, img_height = 200, 300
 
     train_data_dir = '../data/train'
-    validation_data_dir = '../data/test'
+    # validation_data_dir = '../data/test'
     sean_data_dir = '../data/sean_test'
     nb_train_samples = 2684
     nb_validation_samples = 390
@@ -83,15 +83,15 @@ if __name__ == "__main__":
         input_shape = (img_width, img_height, 1)
 
     model = Sequential()
-    model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+    model.add(Conv2D(32, (3, 3), input_shape=input_shape, padding='same'))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Conv2D(32, (3, 3)))
+    model.add(Conv2D(32, (3, 3), padding='same'))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Conv2D(64, (3, 3)))
+    model.add(Conv2D(64, (3, 3), padding='same'))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
