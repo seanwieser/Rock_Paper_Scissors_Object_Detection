234f918af8cfbcd377e6e27204ad02467d85b1b0
Sean Wieser <seanwieser@gmail.com>
2020-08-27 09:39:40 -0700

    shouldn't be working on master

--- src/rps_scratch.py
+++ src/rps_scratch.py
@@ -41,6 +41,13 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
 from tensorflow.keras.layers import Activation, Dropout, Flatten, Dense
 from tensorflow.keras import backend as K
+import matplotlib.pyplot as plt
+
+def plot_acc_epoch(history):
+    accs = history.history
+    fig, ax = plt.subplots()
+    ax.plot(accs)
+    fig.show()
 
 
 # dimensions of our images.
