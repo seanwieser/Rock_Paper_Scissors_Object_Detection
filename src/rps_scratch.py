25ba5b25d6ace97ae5d45d486128959865d6d02a
Sean Wieser <seanwieser@gmail.com>
2020-08-27 22:53:43 -0700

    confusion matrix

--- src/rps_scratch.py
+++ src/rps_scratch.py
@@ -43,19 +43,45 @@ from tensorflow.keras.layers import Activation, Dropout, Flatten, Dense
 from tensorflow.keras import backend as K
 import matplotlib.pyplot as plt
 from os import path  
+from sklearn.metrics import confusion_matrix
+import itertools
+
+def plot_confusion_matrix(cm, classes, title='Confusion matrix', cmap=plt.cm.Blues):
+    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    plt.figure(figsize=(10,10))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
+    plt.savefig('cm.png')
+
 
 def plot_history(history):
     plt.figure(figsize=(20,10))
     plt.subplot(1, 2, 1)
     plt.suptitle('Optimizer : Adam', fontsize=10)
     plt.ylabel('Loss', fontsize=16)
+    plt.xlabel('Epoch', fontsize=16)
     plt.plot(history.history['loss'], label='Training Loss')
     plt.plot(history.history['val_loss'], label='Validation Loss')
     plt.legend(loc='upper right')
-    plt.savefig('loss.png')
 
     plt.subplot(1, 2, 2)
     plt.ylabel('Accuracy', fontsize=16)
+    plt.xlabel('Epoch', fontsize=16)
     plt.plot(history.history['acc'], label='Training Accuracy')
     plt.plot(history.history['val_acc'], label='Validation Accuracy')
     plt.legend(loc='lower right')
@@ -121,7 +147,8 @@ if __name__ == "__main__":
         batch_size=batch_size,
         subset='training',
         color_mode='grayscale',
-        class_mode='categorical')
+        class_mode='categorical',
+        shuffle=True)
 
     validation_generator = datagen.flow_from_directory(
         train_data_dir,
@@ -129,7 +156,8 @@ if __name__ == "__main__":
         batch_size=batch_size,
         subset='validation',
         color_mode='grayscale',
-        class_mode='categorical')
+        class_mode='categorical',
+        shuffle=False)
 
     if path.exists('../data/model_data/rps_weights_scratch.h5'):
         model.load_weights('../data/model_data/rps_weights_scratch.h5')
@@ -150,3 +178,16 @@ if __name__ == "__main__":
     grade = model.evaluate(x=sean_generator)
 
     print(grade)
+
+
+    # compute predictions
+    predictions = model.predict(generator=validation_generator)
+    y_pred = [np.argmax(probas) for probas in predictions]
+    y_test = validation_generator.classes
+    class_names = validation_generator.class_indices.keys()
+    # compute confusion matrix
+    cnf_matrix = confusion_matrix(y_test, y_pred)
+    np.set_printoptions(precision=2)
+
+    # plot normalized confusion matrix
+    plot_confusion_matrix(cnf_matrix, classes=class_names, title='Normalized confusion matrix')
